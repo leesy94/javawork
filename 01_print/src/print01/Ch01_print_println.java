3b9d286319648551151f7dda12114293b4323027
leesy94 <zen294123@gmail.com>
2023-10-18 13:19:00 +0900

    01 print & println
    print : 줄 바꾸지 않음
    println :  줄 바꿈
    주석

---
+++ 01_print/src/print01/Ch01_print_println.java
@@ -0,0 +1,22 @@
+package print01;
+
+public class Ch01_print_println {
+
+	public static void main(String[] args) {
+		//헬로 데얼
+		/* 
+		 할줄 주석 ctrl + / (토글)
+		 여러줄 주석 ctrl + shift + / 
+		 여러줄 주석 해제 ctrl + shift + \
+		 ln => 라인 줄 바꿈
+		*/
+		
+		 System.out.println("이시영"); 
+		 System.out.println("과정명 : 엘라스틱서치 로그 빅데이터");
+		 
+		 System.out.print("줄 바꾸지 않음 ");
+		 System.out.print("강의실"); 
+	}
+
+
+}
