f1483d02162b35738acf253aa1a5defe497369fd
leesy94 <zen294123@gmail.com>
2023-10-18 13:57:19 +0900

    02. println string

---
+++ 01_print/src/print01/Ch02_println_String.java
@@ -0,0 +1,14 @@
+package print01;
+
+public class Ch02_println_String {
+
+	public static void main(String[] args) {
+		System.out.println("문자열, " + "문자열2");
+		System.out.println("문자열, " + 10 + 20); // "" 있고 그 뒤론 무조건 문자열
+		
+		System.out.println(10 + 20 + " ,문자열"); 
+		System.out.println(10 + 20 + " ,문자열, " + (20 - 10)); 
+		
+	}
+
+}
---
+++ 01_print/src/print01/Ch03_println_int.java
@@ -0,0 +1,10 @@
+package print01;
+
+public class Ch03_println_int {
+
+	public static void main(String[] args) {
+		System.out.println(50*34324342);
+		// +, - 는 앞에 문자열이 있으면 무조건 괄호
+	}
+
+}
