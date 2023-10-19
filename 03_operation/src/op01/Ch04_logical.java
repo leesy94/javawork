62247501a9ec3d0903ccd5178af8ad457e5883af
leesy94 <zen294123@gmail.com>
2023-10-19 17:55:27 +0900

    Update Ch04_logical.java

--- 03_operation/src/op01/Ch04_logical.java
+++ 03_operation/src/op01/Ch04_logical.java
@@ -3,8 +3,19 @@ package op01;
 public class Ch04_logical {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// 논리합 , 논리곱
+		// &&(논리합) => 두 조건 모두 만족해야됨
+		// ||(논리곱) => 또는, 둘 중 하나 참 이면 참
+		
+		
+		/*
+		 * 비트연산자
+			2 | 3 = 3
+			00000010
+			00000011
+			--------
+			00000011 => 3
+		*/
 	}
 
 }
