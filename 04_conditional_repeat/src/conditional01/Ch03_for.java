e5a976ec0612684ad20625f71abba2685dc0fd6a
leesy94 <zen294123@gmail.com>
2023-10-20 17:49:05 +0900

    3일째.for문

--- 04_conditional_repeat/src/conditional01/Ch03_for.java
+++ 04_conditional_repeat/src/conditional01/Ch03_for.java
@@ -1,19 +1,72 @@
 package conditional01;
 
+import java.util.Scanner;
+
 public class Ch03_for {
 
 	public static void main(String[] args) {
 		for(int i = 2 ; i <= 9 ; i++) {
+			System.out.print("<"+i + "단> ");
+		}
+		System.out.println(" ");
+		for(int i = 1 ; i <= 9 ; i++) {
 			if(i % 3 == 0) {
 				//System.out.println(i);
 			}
-			System.out.println("<"+i + "단>");
-			for(int j = 1 ; j <= 9 ; j++ ) {
-				System.out.println(i+"x"+j+"="+(i*j));
+			for(int j = 2 ; j <= 9 ; j++ ) {
+				System.out.print(j+"x"+i+"="+(i*j)+" ");
 			}
-			System.out.println( );
+			System.out.println(" ");
 			
 		}
+		
+		
+		System.out.println("---------------------------------");
+		System.out.println("1번.");
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("숫자1 : ");
+		int a = scan.nextInt();
+		System.out.print("숫자2 : ");
+		int b = scan.nextInt();
+		
+		int sum = 0;
+		
+		if(a > b) {
+			//System.out.println("b가 더 작다");
+			for(int i = b ; i <= a ; i++) {				
+				sum += i;
+			}
+			System.out.println(a+"~" + b + "의 합계 : " + sum);
+			
+		}else {
+			//System.out.println("b가 더 크다");
+			for(int i = a ; i <= b ; i++) {	
+				sum += i;
+			}
+			System.out.println(a+"~" + b + "의 합계 : " + sum);
+			
+		}
+		
+		System.out.println("---------------------------------");
+		System.out.println("2번.");
+		
+		System.out.print("숫자1 : ");
+		int a1 = scan.nextInt();
+		if(a1 > 0) {
+			for(int i = 0 ; i <a1 ; i++) {
+				for(int j = 0 ; j <i ; j++) {
+					System.out.print("*");
+				}
+				System.out.println( );
+			}
+			
+		}else {
+			System.out.println("정수가 아닙니다.");
+		}
+		
+
+		
 		
 		
 		
